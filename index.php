52837be520a06541eb69aa387cc76cba4e53ba12
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-24 15:15:55 +0100

    added text without censure

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
+
 ?>
 
 <!DOCTYPE html>
@@ -9,10 +11,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP</title>
 </head>
 
 <body>
+
+    <div>
+        <h1>Testo senza censura</h1>
+        <p><?php echo $text ?></p>
+    </div>
 
 </body>
 
